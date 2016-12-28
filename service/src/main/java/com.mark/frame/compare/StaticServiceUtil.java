f8aab417c9069788217551f778af6c0d8a365e6a
lulei11 <lulei@jd.com>
2016-12-28 14:24:38 +0800

    添加比对程序

---
+++ service/src/main/java/com.mark/frame/compare/StaticServiceUtil.java
@@ -0,0 +1,13 @@
+package com.mark.frame.compare;
+
+/**
+ * Created by lulei on 2016/12/28.
+ * spring静态方法注入
+ */
+public class StaticServiceUtil {
+    static String name;
+
+    public void setName(String inName) {
+        name = inName;
+    }
+}
