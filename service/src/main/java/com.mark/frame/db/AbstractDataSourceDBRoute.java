c6449cb7aed6493905263ab7dd808ad35d57c2be
lulei11 <lulei@jd.com>
2017-03-07 15:47:10 +0800

    test

--- service/src/main/java/com.mark/frame/db/AbstractDataSourceDBRoute.java
+++ service/src/main/java/com.mark/frame/db/AbstractDataSourceDBRoute.java
@@ -1,6 +1,8 @@
 package com.mark.frame.db;
 
 import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -11,6 +13,13 @@ public class AbstractDataSourceDBRoute extends DBRouteImpl
 {
     private List<DataSource> list =new ArrayList<DataSource>();
 
-    
+    public Connection getConnectionInner(Integer num) {
+        Integer pos = getConnection(num);
+        try {
+            return list.get(pos).getConnection();
+        } catch (SQLException e) {
+            throw new RuntimeException(e.getMessage());
+        }
+    }
 
 }
--- service/src/main/java/com.mark/frame/db/DBRoute.java
+++ service/src/main/java/com.mark/frame/db/DBRoute.java
@@ -17,4 +17,5 @@ public interface DBRoute {
 
     public Integer getTotalTableCount();
 
+
 }
