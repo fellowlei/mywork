2bba88bcfc402f137e842d659dc3550f32cf7fe2
lulei11 <lulei@jd.com>
2017-03-14 18:47:12 +0800

    test

---
+++ service/src/main/java/com.mark/frame/http/core/v2/LoginHttpUtil.java
@@ -0,0 +1,32 @@
+package com.mark.frame.http.core.v2;
+
+import java.io.IOException;
+
+/**
+ * Created by lulei on 2017/3/14.
+ */
+public class LoginHttpUtil {
+    public static void login(String name, String password) throws IOException {
+        String loginParam = "username=" + name + "&" + "password=" + password;
+        SessionHttpUtil.loginPost("http://test.com/login.action", loginParam);
+        System.out.println("login success");
+    }
+
+    public static String post(String url, String param) throws IOException {
+        String result = SessionHttpUtil.loginPost(url, param);
+        System.out.println(result);
+        return result;
+    }
+
+    public static String get(String url) throws IOException {
+        String result = SessionHttpUtil.get(url);
+        System.out.println(result);
+        return result;
+    }
+
+    public static void main(String[] args) throws IOException {
+        login("mark", "pass");
+        get("http://test.com/display");
+        post("http://test.com/manager", "action=show");
+    }
+}
